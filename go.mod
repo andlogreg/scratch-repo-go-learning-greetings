d9da2999e683c5f06201fa8891fe347d4e49199c
andlogreg <120428961+andlogreg@users.noreply.github.com>
2024-08-11 18:41:08 +0100

    feat: simple Hello function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/andlogreg/scratch-repo-go-learning-greetings
+
+go 1.22.5
---
+++ greetings.go
@@ -0,0 +1,11 @@
+package greetings
+
+import "fmt"
+
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+    // Return a greeting that embeds the name in a message.
+    message := fmt.Sprintf("Hi, %v. Welcome to my scratch repo!", name)
+    return message
+}
+
